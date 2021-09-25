fcaca5f489370a271d76d425e9260ce01050df20
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-09-26 02:25:11 +0600

    Update Details.js

--- esports-team/src/Components/Details/Details.js
+++ esports-team/src/Components/Details/Details.js
@@ -3,15 +3,17 @@ import React from 'react';
 const Details = (props) => {
     let products=props.details
     let total=0;
+    let name=''
     for(const product of products){
-      
+    name=product.name;
         total=total+product.price;
     }
     return (
-        <div className="pe-5 border border-secondary">
+        <div className="pe-5 border border-secondary w-auto">
             <h1>Details </h1>
-            <h3>Items Orderd :{products.length} </h3>
-            <h4> price:{total} </h4>
+            <h5>name : {name}</h5>
+            <h4 >Items Orderd:{products.length} </h4>
+            <h4>price $:{total} </h4>
         </div>
     );
 };
