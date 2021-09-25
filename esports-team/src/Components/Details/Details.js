9211e7744eb6b1b0a7e4609dd45f663ffb4228ba
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-09-26 03:06:08 +0600

    update

--- esports-team/src/Components/Details/Details.js
+++ esports-team/src/Components/Details/Details.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Details = (props) => {
-    let products=props.details
+
+    let {details}=props
+    
     let total=0;
-    let name=''
-    for(const product of products){
-    name=product.name;
+   
+    
+    for(const product of details){
         total=total+product.price;
+        console.log(product);
+        
     }
     return (
         <div className="pe-5 border border-secondary w-auto">
             <h1>Details </h1>
-            <h5>name : {name}</h5>
-            <h4 >Items Orderd:{products.length} </h4>
+            <ul>
+                {
+                    details.map(game=><li
+                      key={game._id}
+                    >{game.name}</li>)
+                }
+            </ul>
+            <h4 >Items Orderd:{details.length} </h4>
             <h4>price $:{total} </h4>
         </div>
     );
